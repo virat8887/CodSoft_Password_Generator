8241d66271942fe0199dc70b7de07a575a7b69ed
virat8887 <141844538+virat8887@users.noreply.github.com>
2023-08-12 22:02:30 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+def main():
+    print("Password Generator")
+    
+    try:
+        length = int(input("Enter the desired length of the password: "))
+        if length <= 0:
+            print("Password length must be greater than 0.")
+            return
+        
+        password = generate_password(length)
+        print("Generated Password:", password)
+    
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+
+if __name__ == "__main__":
+    main()
